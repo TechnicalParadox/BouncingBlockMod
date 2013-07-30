3d856f544ea58c81fd188e4e59075ca740562889
TechnicalParadox <giamolao98@gmail.com>
2013-07-29 21:41:19 -0400

    Version 1.5

--- tech/bouncingblockmod/BouncingBlock.java
+++ tech/bouncingblockmod/BouncingBlock.java
@@ -7,35 +7,38 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 
-public class BouncingBlock extends Block {
+public class BouncingBlock extends Block
+{
 
-	public BouncingBlock(int par1, Material par2Material) {
-		super(par1, par2Material);
+	public BouncingBlock(int bouncingBlockID, Material par2Material)
+	{
+		super(bouncingBlockID, par2Material);
 		
-		// Variables for the block
+		// Variables for Block
 		this.setHardness(1.0F);
 		this.setStepSound(Block.soundSnowFootstep);
 		this.setUnlocalizedName("bouncingBlock");
 		this.setCreativeTab(CreativeTabs.tabBlock);
-		this.setResistance(1F);	
+		this.setResistance(1.0F);
 	}
-
-	// Block Texture
-	public void registerIcons(IconRegister reg) {
+	
+	// Texture
+	public void registerIcons(IconRegister reg)
+	{
 		this.blockIcon = reg.registerIcon("bouncingblockmod:bouncing_block");
 	}
-	
-	// Player Bounces when block is stepped on
-    public void onEntityWalking(World par1World, int par2, int par3, int par4, Entity par5Entity)
-    {
-        par5Entity.motionY += 2.9d;
-    }
-    
-    // Players fall distance set to 0 when fallen on block
-    public void onFallenUpon(World par1World, int par2, int par3, int par4, Entity par5Entity, float par6)
-    {
-    	par5Entity.fallDistance=0;
-    }
-	
 
+	// Causes the Player to bounce when the block is stepped on
+	public void onEntityWalking(World par1World, int par2, int par3, int par4, Entity par5Entity)
+	{
+		par5Entity.motionY += 2.9d;
+	}
+	
+	// Causes the Player to bounce and not take fall damage when fallen on
+	public void onFallenUpon(World par1World, int par2, int par3, int par4, Entity par5Entity, float par6)
+	{
+		par5Entity.motionY += 2.9d;
+		par5Entity.fallDistance=0;
+	}
+	
 }
--- tech/bouncingblockmod/PaddingBlock.java
+++ tech/bouncingblockmod/PaddingBlock.java
@@ -7,30 +7,31 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 
-public class PaddingBlock extends Block {
+public class PaddingBlock extends Block
+{
 
-	public PaddingBlock(int par1, Material par2Material) {
-		super(par1, par2Material);
+	public PaddingBlock(int paddingBlockID, Material par2Material)
+	{
+		super(paddingBlockID, par2Material);
 		
-		// Variables for the block
+		// Variables for Block
 		this.setHardness(1.0F);
 		this.setStepSound(Block.soundSnowFootstep);
 		this.setUnlocalizedName("paddingBlock");
 		this.setCreativeTab(CreativeTabs.tabBlock);
-		this.setResistance(1F);
-		
+		this.setResistance(1.0F);
 	}
 	
 	// Block Texture
-	public void registerIcons(IconRegister reg) {
+	public void registerIcons(IconRegister reg)
+	{
 		this.blockIcon = reg.registerIcon("bouncingblockmod:padding_block");
 	}
-
-    // Players fall distance set to 0 when fallen on block
-    public void onFallenUpon(World par1World, int par2, int par3, int par4, Entity par5Entity, float par6)
-    {
-    	par5Entity.fallDistance=0;
-    }
 	
+	// Stops fall damage
+	public void onFallenUpon(World par1World, int par2, int par3, int par4, Entity par5Entity, float par6)
+	{
+		par5Entity.fallDistance=0;
+	}
 
 }
