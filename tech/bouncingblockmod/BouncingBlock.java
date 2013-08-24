546622d8f3deea4953fb04730d0807f4b620b633
TechnicalParadox <giamolao98@gmail.com>
2013-08-24 19:31:55 -0400

    Adds comments, still broken algorithm

--- tech/bouncingblockmod/BouncingBlock.java
+++ tech/bouncingblockmod/BouncingBlock.java
@@ -34,16 +34,17 @@ public class BouncingBlock extends Block
 	// Causes the Player to not take fall damage and bounce when fallen on
 	public void onFallenUpon(World par1World, int par2, int par3, int par4, Entity par5Entity, float par6)
 	{
-		FallDistance = par5Entity.fallDistance;
+		FallDistance = par5Entity.motionY; // Would be negative because of falling
+		FallDistance *= -1; // Would change to a positive value
 		
-		par5Entity.fallDistance = 0;
+		par5Entity.fallDistance = 0; // Stops fall damage
 		
-		BounceHeight = FallDistance + .1;
-		if (BounceHeight >= 2.9) {
+		BounceHeight = FallDistance + .1; // Adds a value of .1 to the bounce height each time fallen
+		if (BounceHeight > 2.9) { // If the bounce height is higher than 2.9 (about 34 blocks) it keeps it at 2.9
 			BounceHeight = 2.9;
 		}
 		
-		par5Entity.motionY += BounceHeight;
+		par5Entity.motionY += BounceHeight; // 'Bounces' the player the height declared in BounceHeight
 	}
 	
 }
