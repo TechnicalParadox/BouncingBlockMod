55b083be8f8e1f4dd555b88a747b60f64f2a1788
Gim949 <jagielski.przemek@yahoo.com>
2013-09-25 17:46:18 -0400

    Added resFolder String and getResFolder method

--- tech/bouncingblockmod/Reference.java
+++ tech/bouncingblockmod/Reference.java
@@ -4,6 +4,10 @@ public class Reference {
 	
 	public static final String MOD_ID = "bouncingblockmod";
 	public static final String MOD_NAME = "Bouncing Block Mod";
-	public static final String MOD_VERSION = "2.0 for MC 1.6.2 (FORGE 9.10.0.804)";
+	public static final String MOD_VERSION = "2.1 for MC 1.6.2 - 1.6.4 (FORGE 9.10.0.804 / 9.11.0.883)";
+	private static String resFolder = "bouncingblockmod:";
 	
+	public static String getResFolder(){
+		return resFolder;
+	}
 }
